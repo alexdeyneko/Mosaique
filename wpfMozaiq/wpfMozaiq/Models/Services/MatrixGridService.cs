e1f9283a868dfe205d4bd2e77d0365e7df7c5331
alexdeyneko <alfire17496@gmail.com>
2017-10-22 01:27:57 +0500

    увеличил скорость генерации в 10 раз

--- wpfMozaiq/wpfMozaiq/Models/Services/MatrixGridService.cs
+++ wpfMozaiq/wpfMozaiq/Models/Services/MatrixGridService.cs
@@ -43,36 +43,60 @@ namespace wpfMozaiq.Models.Services
             int pixelWidth = Image.Picture.Width / width;      //ширина одного блока в пикселях
             Grid = new PixelsBlock[width, height];
 
+            /*
+            for (int i = 0; i < width; i++)
+             {
+                 for (int j = 0; j < height; j++)
+                 {
 
-            //Bitmap newMap = new Bitmap(width*pixelWidth,height*pixelHeight);
-            //Graphics g = Graphics.FromImage(newMap);
+                     PixelsBlock block = new PixelsBlock();
+                     block.Picture = Image.Picture.Clone(new Rectangle
 
+                         (i* pixelWidth, 
+                                    j* pixelHeight, 
+                        pixelWidth, pixelHeight), 
+
+                        Image.Picture.PixelFormat);
+
+                    block.CalculateAvrColors();
+                    
+                    Grid[i, j] = block;
+
+                    
+              
+                }
+
+            }
+            */
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
-
                     PixelsBlock block = new PixelsBlock();
-                    block.Picture = Image.Picture.Clone(new Rectangle
+                    Rectangle section = new Rectangle(i *
+                        pixelWidth, j * pixelHeight,
+                          pixelWidth,
+                       pixelHeight
+               );
 
-                        (i * pixelWidth,
-                                   j * pixelHeight,
-                       pixelWidth, pixelHeight),
 
-                       Image.Picture.PixelFormat);
+                    Bitmap bmp = new Bitmap(section.Width, section.Height);
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.DrawImage(Image.Picture, 0, 0, section, GraphicsUnit.Pixel);
+                    }
+
+                    block.Picture = bmp;
 
                     block.CalculateAvrColors();
 
                     Grid[i, j] = block;
 
-                    //g.DrawImage(block.Picture, i * pixelWidth, j * pixelHeight, pixelWidth, pixelHeight);
+
 
                 }
 
             }
-            //g.Dispose();
-
-            //newMap.Save("C:\\Users\\Алексей\\Desktop\\пам5.bmp");
 
         }
     }
