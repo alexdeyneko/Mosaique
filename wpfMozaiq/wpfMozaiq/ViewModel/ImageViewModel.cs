7d4d3620a76dad3c683196035fd11765e6fac1aa
AlekseyKonovalov <konov.95@mail.ru>
2017-11-07 22:51:59 +0500

    сделано масштабирование на вьюхе панно

--- wpfMozaiq/wpfMozaiq/ViewModel/ImageViewModel.cs
+++ wpfMozaiq/wpfMozaiq/ViewModel/ImageViewModel.cs
@@ -4,7 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using wpfMozaiq.Veiw;
 
 namespace wpfMozaiq.ViewModel
 {
@@ -24,10 +27,65 @@ namespace wpfMozaiq.ViewModel
 		    }
 	    }
 
-	    public ImageViewModel()
+	    private int _imageWidth;
+	    public int ImageWidth
+		{
+		    set
+		    {
+			    _imageWidth = value;
+			    RaisePropertyChanged(() => ImageWidth);
+		    }
+		    get
+		    {
+			    return _imageWidth;
+		    }
+	    }
+
+	    private int _imageHeight;
+	    public int ImageHeight
+	    {
+		    set
+		    {
+			    _imageHeight = value;
+			    RaisePropertyChanged(() => ImageHeight);
+		    }
+		    get
+		    {
+			    return _imageHeight;
+		    }
+	    }
+
+		public ImageViewModel()
 	    {
 			ImagePath = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())) + "\\Temp\\" + "tempImage.bmp";
-		    int i = 0;
+		    ImageHeight = 400;
+		    ImageWidth = 400;
+
+		}
+
+	    private ICommand _pressPlus;
+	    public ICommand PressPlus
+		{
+		    get => _pressPlus ?? (_pressPlus = new RelayCommand(() =>
+		    {
+			    ImageHeight += 25;
+			    ImageWidth += 25;
+			    int i = 0;
+
+		    }));
 	    }
+
+	    private ICommand _pressMinus;
+	    public ICommand PressMinus
+		{
+		    get => _pressMinus ?? (_pressMinus = new RelayCommand(() =>
+			{
+				ImageHeight -= 25;
+				ImageWidth -= 25;
+				int i = 0;
+			}));
+	    }
+
+
 	}
 }
